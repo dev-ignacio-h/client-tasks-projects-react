10b9f3d1f78994e3d419e511ce0818b7bc00a4ad
Ignacio <dev.ignacio.h@gmail.com>
2021-08-24 12:14:54 -0400

    initial components login, register, projects and folders

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  // State para iniciar sesión
+  const [usuario, guardarUsuario] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = usuario;
+
+  const onChange = (e) => {
+    // guardarUsuario({
+    //   ...usuario,
+    //   [e.target.name]: e.target.value
+    // })
+    guardarUsuario((prevState) => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value
+      };
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    // validar que no haya campos vacios
+
+    // pasarlo al action
+  };
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Iniciar Sesión</h1>
+        <form onSubmit={onSubmit}>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Tu Email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Tu Password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Iniciar Sesión"
+            />
+          </div>
+        </form>
+
+        <Link to={'/register'} className="enlace-cuenta">
+          Obtener Cuenta
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,5 @@
+const Projects = () => {
+  return <h1>From Projects</h1>;
+};
+
+export default Projects;
